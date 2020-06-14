f2f2efecefb6578b88f7cb5ae17ace5533030739
beven.zhou@hotmail.com <beven.zhou@hotmail.com>
2020-06-14 08:37:28 +0800

    添加用例

---
+++ src/hot/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @module HotReload
+ */
+export default class HotReload {
+  /**
+   * 启动controller级别热更新
+   */
+  watch() {
+
+  }
+}
---
+++ test/models/UserInfo.ts
@@ -0,0 +1,18 @@
+import { ApiModel, ApiModelProperty } from '../../index';
+
+@ApiModel({ value: '用户信息', description: '用户信息。。' })
+export default class UserInfo {
+
+  static from(data) {
+    const user = new UserInfo();
+    user.userName = data.userName;
+    return user;
+  }
+
+  @ApiModelProperty({ value: '用户名', required: true, example: '张三' })
+  public userName: string
+
+
+  @ApiModelProperty({ value: '用户编码', required: true, example: 1 })
+  public userId: number
+}
