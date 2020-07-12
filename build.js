72ad3b5aa66a87cf669e495516233616cf5e8963
beven.zhou@hotmail.com <beven.zhou@hotmail.com>
2020-07-12 18:15:36 +0800

    添加构建命令

---
+++ build.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const fs = require('fs-extra');
+
+const assets = [
+  'src/swagger/swagger-ui',
+];
+
+const tasks = [
+  clean,
+  copy,
+];
+
+function clean() {
+  console.log('Clean dist');
+  fs.removeSync(path.resolve('dist'));
+}
+
+function copy() {
+  assets.forEach((dir) => {
+    const from = path.resolve(dir);
+    const dest = path.resolve('dist/' + dir);
+    console.log('Copy ', dir);
+    fs.copySync(from, dest);
+  });
+}
+
+
+tasks.forEach((handler) => handler());
