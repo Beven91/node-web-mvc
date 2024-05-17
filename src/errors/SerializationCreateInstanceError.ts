01ab641f7fe79e030f3e35d05fb919d30a648f29
beven <beven.zhou@hotmail.com>
2024-05-17 15:06:33 +0800

    add SerializationCreateInstanceError

---
+++ src/errors/SerializationCreateInstanceError.ts
@@ -0,0 +1,10 @@
+import Exception from "./Exception";
+
+
+export default class SerializationCreateInstanceError extends Exception {
+
+  constructor(type: Function, message: string) {
+    super(`Cannot create ${type.name} instance fail ` + message)
+  }
+
+}
