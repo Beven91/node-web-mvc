d5430408925fd670cc43474974854ef5a31e4b00
beven <beven.zhou@hotmail.com>
2024-05-28 13:21:12 +0800

    remove testcode

--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,4 @@
-import { MediaType, Registry } from '../src';
+import { Registry } from '../src';
 import WebAppConfigurer from './WebAppConfigurer';
-
-console.log(new MediaType('multipart/form-data; boundary=----WebKitFormBoundaryLFuqAAPatykTLKer'));
 
 Registry.launch(new WebAppConfigurer());
