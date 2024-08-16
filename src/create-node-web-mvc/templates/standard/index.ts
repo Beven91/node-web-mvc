677e1aebe5a74cf1681c57cfac35e8591e81ad0c
beven <beven.zhou@hotmail.com>
2024-08-16 15:16:04 +0800

    调整node-web-mvc构建时关闭sourceMap

--- src/create-node-web-mvc/templates/standard/index.ts
+++ src/create-node-web-mvc/templates/standard/index.ts
@@ -1,6 +1,7 @@
 import { SpringApplication, SpringBootApplication } from 'node-web-mvc';
 
 @SpringBootApplication({
+  swagger: process.env.NODE_ENV !== 'production',
   // 代码热更新： 在该目录下的文件改动支持热更新（无需重启服务) 注意：在process.env.NODE_ENV === 'production'时强制无效
   hot: './src',
   // 启动时需要加载的模块目录， 在不配置时默认为 process.cwd()
