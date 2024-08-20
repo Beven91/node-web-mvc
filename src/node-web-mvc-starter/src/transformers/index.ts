0fa456f5cfdd2277f79a561b06c242f67e3f5396
beven <beven.zhou@hotmail.com>
2024-08-20 14:42:45 +0800

    remove hot-update-transformer

--- src/node-web-mvc-starter/src/transformers/index.ts
+++ src/node-web-mvc-starter/src/transformers/index.ts
@@ -1,11 +1,9 @@
 import ts from 'typescript';
 import enhanceTypeTransformer from './enhanceTypeTransformer';
-import removeHotUpdateTransformer from './removeHotUpdateTransformer';
 
 export function createTransformers(program: ts.Program, isProd: boolean) {
   const prodTransfermers: ts.CustomTransformers = {
     before: [
-      (context)=> removeHotUpdateTransformer(context, program),
     ],
     after: [],
   };
