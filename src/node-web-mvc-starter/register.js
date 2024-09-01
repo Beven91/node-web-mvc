92727e68c28512b9cc999e86d3de069a19ca2cff
beven <beven.zhou@hotmail.com>
2024-09-01 11:56:20 +0800

    optimize: register

--- src/node-web-mvc-starter/register.js
+++ src/node-web-mvc-starter/register.js
@@ -3,7 +3,10 @@ const path = require('path');
 const { parseOptions } = require('./dist/build');
 const dev = require('./dist/dev').default;
 
-const entry = process.argv[1] || process.cwd();
+const entry = process.argv.find((m)=>{
+  const ext = path.extname(m);
+  return ext == '.ts' || ext == '.tsx';
+}) || process.cwd();
 const options = parseOptions('dev', entry, process.argv.slice(2));
 
 dev(options);
