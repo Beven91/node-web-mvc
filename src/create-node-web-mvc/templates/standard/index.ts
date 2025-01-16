7f3cf217bb0030c040f54ff343e8edf25f1de659
beven <beven.zhou@hotmail.com>
2025-01-16 11:28:33 +0800

    create-node-web-mvc: 调整模板热更新监听目录

--- src/create-node-web-mvc/templates/standard/index.ts
+++ src/create-node-web-mvc/templates/standard/index.ts
@@ -3,7 +3,10 @@ import { SpringApplication, SpringBootApplication } from 'node-web-mvc';
 @SpringBootApplication({
   swagger: process.env.NODE_ENV !== 'production',
   // 代码热更新： 在该目录下的文件改动支持热更新（无需重启服务) 注意：在process.env.NODE_ENV === 'production'时强制无效
-  hot: './src',
+  hot: [
+    './src',
+    './index.ts'
+  ],
   // 启动时需要加载的模块目录， 在不配置时默认为 process.cwd()
   scanBasePackages: './src',
   // 配置服务端口相关
